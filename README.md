0630c3c8033a3887181bfd9abc1156217077206d
Christian Heilmann <chris.heilmann@gmail.com>
2023-04-29 22:17:49 +0200

    new

---
+++ README.md
@@ -0,0 +1,7 @@
+# Browser Entwickler Tools für Nicht-Entwickler:Innen
+
+Demoseiten für den "Browser Entwickler Tools für Nicht-Entwickler:Innen" Kurs auf LinkedIn Learning.
+
+Zum selbst ausprobieren:
+
+[https://codepo8.github.io/devtools-deutsch/demos/](https://codepo8.github.io/devtools-deutsch/demos/)
---
+++ demos/assets/nav.js
@@ -0,0 +1,78 @@
+let allurls = {
+    "console": [
+            {u:"scripts.html",t: "Creating own scripts"},
+    ],
+    "network": [
+            {u:"gallery.html",t: "Images in document"},
+            {u:"background-gallery.html",t: "Background images"},
+            {u:"video.html",t: "Simpler videos"}
+    ],
+    "cookies": [
+        {u:"cookie-demo.html",t: "Cookie Demo"}
+    ],
+    "issues": [
+        {u:"page-with-errors.html",t: "Webpage with errors"}
+    ],
+    "elements": [
+        {u:"elements.html",t: "Changing Elements"},
+        {u:"deleting-elements.html",t: "Deleting Elements"},
+        {u:"overlay.html",t: "Blocked web page"},
+        {u:"timed-button.html",t: "Blocked button"},
+        {u:"style-changes.html",t: "Change styles"}
+    ],
+    "inspector": [
+        {u:"inspector-demo.html",t: "Quick Info"},
+        {u:"context-menu-blocker.html",t: "Blocked Context Menu"},
+        {u:"elements-without-space.html",t: "Elements without much space"},
+    ],
+    "screenshots": [
+        {u:"long-page.html",t: "Long page"}
+    ],
+    "simulation": [
+        {u:"mobile-version.html",t: "Picture Gallery"},
+        {u:"simpler-mobile-video.html",t: "Mobile Videos"}
+    ]
+};
+
+
+let current = document.location.href.split('/').pop();
+let chunks = document.location.href.split('/');
+let urls = [];
+urls.push({u:"index.html",t: "Intro"});
+urls = urls.concat(allurls[chunks[chunks.length-2]]);
+urls.push({u:"review.html",t: "Review"});
+urls.push({u:"../index.html",t: `<svg width="1.3em" height="1.3em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11.3103 1.77586C11.6966 1.40805 12.3034 1.40805 12.6897 1.77586L20.6897 9.39491L23.1897 11.7759C23.5896 12.1567 23.605 12.7897 23.2241 13.1897C22.8433 13.5896 22.2103 13.605 21.8103 13.2241L21 12.4524V20C21 21.1046 20.1046 22 19 22H14H10H5C3.89543 22 3 21.1046 3 20V12.4524L2.18966 13.2241C1.78972 13.605 1.15675 13.5896 0.775862 13.1897C0.394976 12.7897 0.410414 12.1567 0.810345 11.7759L3.31034 9.39491L11.3103 1.77586ZM5 10.5476V20H9V15C9 13.3431 10.3431 12 12 12C13.6569 12 15 13.3431 15 15V20H19V10.5476L12 3.88095L5 10.5476ZM13 20V15C13 14.4477 12.5523 14 12 14C11.4477 14 11 14.4477 11 15V20H13Z"/>
+</svg>`});
+let secondaryurls = allurls[chunks[chunks.length-2]];
+
+let nav = document.querySelector("nav ul");
+urls.forEach(u => {
+    let li = document.createElement("li");
+    if (u.u !== current) {
+        let a = document.createElement("a");
+        a.href = u.u;
+        a.innerHTML = u.t;
+        li.appendChild(a);
+    } else {
+        li.classList.add('current');
+        li.textContent = u.t;
+    }
+    nav.appendChild(li);
+});
+// let secondary = document.querySelector("#secondary");
+// let out = '<ul>';
+// secondaryurls.forEach(u => {
+//     out += `<li><a href="${u.u}">${u.t}</a></li>`;
+// });
+// out += '</ul>';
+// secondary.innerHTML = out;
+
+let demourl = document.querySelector(".demourl a");
+if (demourl) {
+    let url = 'codepo8.github.io/devtools/demos';
+    let addon = demourl.innerText.split('/');
+    addon = addon[addon.length-2];
+    demourl.innerText = url + '/' + addon + '/';
+    demourl.href = 'https://'+url+'/' + addon;
+}
